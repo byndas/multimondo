1c7911a589fbce8e6a350cf1f975ad7ca86a6c2e
marcByndas <byndasmarc@gmail.com>
2022-10-19 16:02:20 -0400

    main branch up to date

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { customButtonContainer } from "./custom-button.styles";
-
-const CustomButton = ({ children, ...props }) => (
-  <customButtonContainer {...props}>{children}</customButtonContainer>
-);
-
-export default CustomButton;
